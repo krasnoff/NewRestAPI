0f07e9fb5ecff4dd0cb97f61a395ac6a7fffd4ea
krasnof.kobi@gmail.com <krasnoff.kobi@gmail.com>
2018-07-30 13:26:13 +0300

    start install seurity

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace NewRestAPI.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    public class LoginController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public LoginController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        
+        // POST api/values
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Post([FromBody] TokenRequest request)
+        {
+            if (request.Username == "Kobi" && request.Password == "1111")
+            {
+                var claims = new[]
+                {
+                    new Claim(ClaimTypes.Name, request.Username)
+                };
+
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["SecurityKey"]));
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var token = new JwtSecurityToken(
+                    issuer: "yourdomain.com",
+                    audience: "yourdomain.com",
+                    claims: claims,
+                    expires: DateTime.Now.AddMinutes(30),
+                    signingCredentials: creds);
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token)
+                });
+            }
+
+            return BadRequest("Could not verify username and password");
+        }
+    }
+
+    public class TokenRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
